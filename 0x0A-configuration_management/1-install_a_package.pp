a267f1eead50e037f8079bb9918dc55c9050694a
KayBTee <kbthaba@gmail.com>
2024-07-03 07:15:11 -0700

    1 install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,5 @@
-#!/usr/bin/pup
-#Installing flask from pip3
-package { 'python3-pip':
-  ensure   => installed,
-}
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
-  require  => package['python3-pip'],
-}
-
-package { 'werkzeug':
-  ensure   => '2.0.1',
-  provider => 'pip',
-  require  => package['python3-pip'],
-}
+# install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
